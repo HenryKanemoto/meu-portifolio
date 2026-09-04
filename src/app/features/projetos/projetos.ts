f3fe76b4b51429b665481f3592169798ab34a411
Henry Kanemoto <kanemotohenry@gmail.com>
2026-09-04 14:40:57 -0300

    feat (features): Cria componente de projetos

---
+++ src/app/features/projetos/projetos.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  imports: [],
+  standalone: true,
+  selector: 'app-projetos',
+  styleUrl: './projetos.css',
+  templateUrl: './projetos.html',
+})
+export class Projetos {}
