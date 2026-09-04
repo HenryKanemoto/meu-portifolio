222eafc1248bbd44cbc5c26b38c7f184ebfea730
Henry Kanemoto <kanemotohenry@gmail.com>
2026-09-04 14:41:28 -0300

    feat (features): Adiciona lógica de rotas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,21 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        redirectTo: 'home',
+        pathMatch: 'full',
+    },
+    {
+        path: 'home',
+        loadComponent: () => import('./features/home/home').then(m => m.Home)
+    },
+    {
+        path: 'qualificacoes',
+        loadComponent: () => import('./features/qualificacao/qualificacao').then(m => m.Qualificacao)
+    },
+    {
+        path: 'projetos',
+        loadComponent: () => import('./features/projetos/projetos').then(m => m.Projetos)
+    }
+];
